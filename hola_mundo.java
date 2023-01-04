35177ba0043820fcb605294702633e6226cadd45
jaime <jaimeriquelmegarcia@gmail.com>
2023-01-04 10:28:47 +0100

    4 mensajes

--- hola_mundo.java
+++ hola_mundo.java
@@ -4,5 +4,9 @@ public class hola_mundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo!");
     }
 }
